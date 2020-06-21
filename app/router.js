3c5a14390a19eda2f83e042ebf71370c91556f52
Simar Singh <simar@codejudge.io>
2020-06-22 01:32:58 +0530

    fixes for sonar

--- app/router.js
+++ app/router.js
@@ -1,6 +1,5 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
-import ENV from './config/environment';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
@@ -8,7 +7,7 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-  console.log(ENV.API_URL);
+  console.log(config.API_URL);
 });
 
 export default Router;
--- config/environment.js
+++ config/environment.js
@@ -25,12 +25,6 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     ENV.API_URL =  'http://localhost:8080';
-
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
